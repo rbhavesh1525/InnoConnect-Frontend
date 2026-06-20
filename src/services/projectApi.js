930403ba449d45741ce0c2992056b52e0ebef1e1
satyam-trimale <trimalesatyam@gmail.com>
2026-06-20 15:49:57 +0530

    Enhanced post project logic

--- src/services/projectApi.js
+++ src/services/projectApi.js
@@ -2,6 +2,14 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
+function getAuthHeaders() {
+  const token = localStorage.getItem("token");
+  return {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+}
+
 export async function checkSimilarity(project) {
   const res = await axios.post(`${API_URL}/similarity`, project, {
     headers: { "Content-Type": "application/json" },
@@ -11,7 +19,7 @@ export async function checkSimilarity(project) {
 
 export async function submitProject(project) {
   const res = await axios.post(`${API_URL}/submit-project`, project, {
-    headers: { "Content-Type": "application/json" },
+    headers: getAuthHeaders(),
   });
   return res.data;
 }
